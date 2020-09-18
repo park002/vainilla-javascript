e58fbca0daf5a859d51ad1ee75000dfd21c2002b
ekem159 <ekem159@naver.com>
2020-09-18 14:56:45 +0900

    momentum dropdown.js update

--- momentum/js/dropdown.js
+++ momentum/js/dropdown.js
@@ -1,29 +1,34 @@
-const links = document.querySelector('.js-header__links'),
-    addLink = document.querySelector('.js-header__add-link'),
+const links = document.querySelector('#js-header__links'),
     dropDownSection = document.querySelector('.header__dropdown-section'),
     dropDownDetail = document.querySelector('.header__dropdown-detail');
 
 
-//const header__link-hide;
-
-function handleDropDown(event) {
-
-    // dropDownSection.classList.add(SHOWING_CLASS);
-    // event.target.classList.add(CHECK_CLASS); //links
-    // const linksTarget = event.target.classList.contains(CHECK_CLASS);
-    // body.addEventListener("click", function (event) {
-    //     if (event.target.classList.contains(CHECK_CLASS) !== linksTarget) {
-    //         dropDownSection.classList.remove(SHOWING_CLASS);
-    //     }
-    //     else { //둘다 check 클래스를 가지고있다면
-    //         dropDownSection.classList.add(SHOWING_CLASS);
-    //     }
-    // })
+function handleActivation(event) {
+    const target = event.target;
+    console.log(target);
+    if (target.id === 'js-header__links') {
+        dropDownSection.classList.toggle(SHOWING_CLASS); 
+    }
+    else if (target.id === 'js-header__add-link'|| target.id === 'js-header__dropdown-container' || target.id === 'js-header__list') {
+        dropDownSection.classList.add(SHOWING_CLASS);
+    }
+    else if (target.id === 'js-header_new-link') {
+        linksAdd();
+    }
+    else {
+        deActivation();
+    }
 }
-function loadLinks() {
-    links.addEventListener("click", handleDropDown);
+function deActivation() {
+    dropDownSection.classList.remove(SHOWING_CLASS);
 }
+
+function activation() {
+    body.addEventListener("click", handleActivation);
+}
+
+
 function init() {
-    loadLinks();
+    activation();
 }
 init();
